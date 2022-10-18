b9cc98e64f101fd3ed959d1eb35e821b6ee6586d
jschlicker <julian.schlicker@freenet.ag>
2022-10-18 19:34:25 +0200

    fix: configure primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { ButtonModule } from "primeng/button";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, ButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
