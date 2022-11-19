c62ab04e96f4833d2ba6ad3023d78b1aaab2604d
jschlicker <julian.schlicker@freenet.ag>
2022-11-19 23:09:03 +0100

    build: add angular library firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { getFunctions, provideFunctions } from "@angular/fire/functions";
 import { ButtonModule } from "primeng/button";
+import { environment } from "../environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ButtonModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ButtonModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideFunctions(() => getFunctions()),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
