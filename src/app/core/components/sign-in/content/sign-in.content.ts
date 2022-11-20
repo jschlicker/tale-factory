b66a9eabe4fb0ba909ef0d50ba1a79ada2438a48
jschlicker <julian.schlicker@freenet.ag>
2022-11-20 14:34:44 +0100

    style: move assets folder to app dir

--- src/app/core/components/sign-in/content/sign-in.content.ts
+++ src/app/core/components/sign-in/content/sign-in.content.ts
@@ -7,17 +7,17 @@ export const CONTENT: SignInContent = [
         title: "Online management",
         description:
             "Tale-Factory offers different ways to create and edit your adventure...from a simple item to a whole story.",
-        img: "../../../../../assets/images/cloud-document-management.svg",
+        img: "../../../../assets/images/cloud-document-management.svg",
     },
     {
         title: "Live sessions",
         description:
             "Thanks to intelligent and far-reaching functionalities, an authentic and exciting gaming experience is created, so that every game round makes fun as much as in real.",
-        img: "../../../../../assets/images/online-live-sessions.svg",
+        img: "../../../../assets/images/online-live-sessions.svg",
     },
     {
         title: "Cloud storage",
         description: "All files are backed up online and can be shared or made publicly available if required.",
-        img: "../../../../../assets/images/publicly-document-providing.svg",
+        img: "../../../../assets/images/publicly-document-providing.svg",
     },
 ];
